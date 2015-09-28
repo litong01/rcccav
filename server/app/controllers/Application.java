d2acbee9686eed8147b1f5956fa8665f121a312d
Tong Li <litong01@us.ibm.com>
2015-09-28 19:46:23 -0400

    implemented doCommand function

--- server/app/controllers/Application.java
+++ server/app/controllers/Application.java
@@ -1,40 +1,43 @@
 package controllers;
 
+import org.json.simple.JSONObject;
+
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 import models.rcccav.*;
 
 
 public class Application extends Controller {
-    
+    private DeviceController controller = DeviceController.getInstance("conf/avconfig.json");
+
     public Result index() {
         return ok("Welcome to RCCC AV!");
     }
-	
-	/**
-	 * Whole system actions
+
+    /**
+     * Whole system actions
      * @param action The system action. Valid values are ON, OFF, REBOOT and FORCE_OFF
      * @return Action result.
      */
     public Result powerSystem(String action){
-		Logger.debug("in powerSystem... action = " + action);
-		DeviceController.getInstance("conf/avconfig.json").powerSystem(DeviceController.SystemAction.valueOf(action));
-		Logger.debug("finished call DeviceController.");
-		return ok("System " + action + " succeeds");
-	
-	}
-	
+        Logger.debug("in powerSystem... action = " + action);
+        this.controller.powerSystem(DeviceController.SystemAction.valueOf(action));
+        Logger.debug("finished call DeviceController.");
+        return ok("System " + action + " succeeds");
+
+    }
+
     /**
-	 * Switch video signal
+     * Switch video signal
      * @param source The source of video signal 
-	 * @param output The destination of video signal
+     * @param output The destination of video signal
      * @return Action result.
      */
-	public Result switchVideoSignal(String source, String output){
-		return ok("Switching video signal from " + source + " to " + output);
-	
-	}
-
+    public Result switchVideoSignal(String source, String output){
+        this.controller.doCommand(deviceName, command);
+        String results = this.controller.getActionResponse(deviceName);
+        return ok("Switching video signal from " + source + " to " + output +
+                  ". Results are " + results);
+    }
 }
