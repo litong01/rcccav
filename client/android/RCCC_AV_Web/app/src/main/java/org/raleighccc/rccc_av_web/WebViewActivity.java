4c687e262f8212112d3a20ec7a1730cf928c5b5e
pinwu748 <pinwu_xu@hotmail.com>
2015-11-04 07:28:27 -0500

    add the Android web client

---
+++ client/android/RCCC_AV_Web/app/src/main/java/org/raleighccc/rccc_av_web/WebViewActivity.java
@@ -0,0 +1,93 @@
+package org.raleighccc.rccc_av_web;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        /*
+        // NOTE: this section works fine
+        String customHtml = "<html><body><h1>Hello, WebView</h1>" +
+                "<h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3>" +
+                "<p>This is a sample paragraph.</p>" +
+                "</body></html>";
+        webView.loadData(customHtml, "text/html", "UTF-8");
+        */
+
+        webView.setWebChromeClient(new WebChromeClient());
+        // ISSUE: we don't have popup windows
+        // webView.setWebViewClient(new MyWebViewClient());
+
+        // TODO: change to 192.168.1.37
+        String url = "http://192.168.1.37/";
+        //String url = "http://www.javatechig.com";
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setSupportMultipleWindows(true);
+        webView.loadUrl(url);
+
+        /*
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        */
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
