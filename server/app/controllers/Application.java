1c71b46283a8c834bc0b816576104bed676cde61
Tong Li <litong01@us.ibm.com>
2015-11-19 15:36:33 -0500

    Fix some outdated comments and function parameters

--- server/app/controllers/Application.java
+++ server/app/controllers/Application.java
@@ -30,17 +30,17 @@ public class Application extends Controller {
     }
 
     /**
-     * Switch video signal
-     * @param source The source of video signal 
-     * @param output The destination of video signal
+     * Perform action on a give device
+     * @param device The AV device which the action will be performed 
+     * @param action The action the device will perform.
      * @return Action result.
      */
-    public Result switchVideoSignal(String source, String output){
+    public Result switchVideoSignal(String device, String action){
         synchronized(this) {
-            this.controller.doCommand(source, output);
-            String results = this.controller.getActionResponse(source);
-            return ok("Perform video switch on device: " + source + ". Action: " +
-                      output + ".<br/>Results: " + results);
+            this.controller.doCommand(device, action);
+            String results = this.controller.getActionResponse(device);
+            return ok("Perform video switch on device: " + device + ". Action: " +
+                      action + ".<br/>Results: " + results);
         }
     }
 }
