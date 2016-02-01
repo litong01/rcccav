ba8c38c49e5ea50c9feb0d4d6a05feef20c07266
Tong Li <litong01>
2016-01-31 23:28:38 -0500

    Added a time test class

---
+++ tests/TestTime.java
@@ -0,0 +1,9 @@
+
+
+public class TestTime
+{
+    public static void main(String[] args) {
+        long timemark = System.currentTimeMillis() - 5184000000L;
+        System.out.println(timemark);
+    }
+}
